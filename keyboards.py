167e3774234c0bf0a40183d49a47f064d8717304
ezmerykhan-netizen <ezmerykhan@gmail.com>
2026-07-30 17:29:30 +0330

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,8 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-def main_menu(lang="fa"):
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("➕ ساخت ربات جدید", callback_data="create_bot")],
-        [InlineKeyboardButton("🤖 ربات‌های من", callback_data="my_bots")],
-        [InlineKeyboardButton("🌐 تغییر زبان", callback_data="change_lang")]
-    ])
