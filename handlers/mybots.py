75f66e2a63966208b21b941fd2690bde1c9af62d
ezmerykhan-netizen <ezmerykhan@gmail.com>
2026-09-17 22:25:45 +0330

    Delete handlers directory

--- handlers/mybots.py
+++
@@ -1,2 +0,0 @@
-async def mybots_handler(update, context):
-    await update.message.reply_text("لیست ربات‌های شما هنوز ساخته نشده ❤️")
--- handlers/start.py
+++
@@ -1,2 +0,0 @@
-async def start_handler(update, context):
-    await update.message.reply_text("سلام میکو ربات آماده‌ست ❤️")
