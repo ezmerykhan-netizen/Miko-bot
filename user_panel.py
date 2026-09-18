8cb2d1bed45453a9527f81c68f2a0dfa40b2a8d8
ezmerykhan-netizen <ezmerykhan@gmail.com>
2026-09-18 14:05:04 +0330

    Update user_panel.py

--- user_panel.py
+++ user_panel.py
@@ -1,9 +1,16 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from texts import BTN_SUPPORT
+import database as db
 
 
-def user_keyboard():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("💰 حمایت مالی", callback_data="donate")],
-        [InlineKeyboardButton(BTN_SUPPORT, callback_data="contact_admin")],
-    ])
+async def user_keyboard():
+    """پنل کاربر شیشه‌ای"""
+    music_on = await db.get_setting("music_enabled", "1") == "1"
+
+    buttons = []
+    if music_on:
+        buttons.append([InlineKeyboardButton("🎵 پیدا کردن آهنگ", callback_data="music_start")])
+    buttons.append([InlineKeyboardButton("💰 حمایت مالی", callback_data="donate")])
+    buttons.append([InlineKeyboardButton(BTN_SUPPORT, callback_data="contact_admin")])
+
+    return InlineKeyboardMarkup(buttons)
